431ff5114d203564aec9aae6c8a1ee8fe764be0c
Sebastian Wolff <bastiwolff432@gmail.com>
2025-05-21 19:18:23 +0200

    added more priddles , added puzzle walls , added ice block wich slides, added single use button at dungeon

--- scripts/puzzle_button.gd
+++ scripts/puzzle_button.gd
@@ -1,20 +1,29 @@
 extends Area2D
 
+@export var is_single_use: bool
+
 var bodies_on_top: int = 0
 
 signal pressed
 signal unpressed
 
+
 func _on_body_entered(body: Node2D) -> void:
+	bodies_on_top += 1
 	if body.is_in_group('pushable') || body is Player:
-		bodies_on_top += 1
-		pressed.emit()
-		$AnimatedSprite2D.play("pressed")
+		if bodies_on_top == 1:
+			pressed.emit()
+			print('now on top')
+			print('Bodies:', bodies_on_top)
+			$AnimatedSprite2D.play("pressed")
 
 
 func _on_body_exited(body: Node2D) -> void:
+	if is_single_use:
+		return
+	bodies_on_top -= 1
 	if body.is_in_group('pushable') || body is Player:
-		bodies_on_top -= 1
-		if bodies_on_top < 1:
+		if bodies_on_top == 0:
+			print('left Bodies:', bodies_on_top)
 			unpressed.emit()
 			$AnimatedSprite2D.play("unpressed")
