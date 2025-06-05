41a3e8b924d668200ce545413278702cae27c43e
Sebastian Wolff <bastiwolff432@gmail.com>
2025-06-05 18:37:46 +0200

    did some better organizing + added new area which will be a city, where the player can buy items and interact with npcs

--- scripts/scene_entrance.gd
+++ scripts/scene_entrance.gd
@@ -2,14 +2,14 @@ extends Area2D
 
 @export var new_scene: String
 
-@export var player_spawn_position: Vector2
+@export var player_other_scene_spawn: Vector2
 
 func _ready() -> void:
 	self.body_entered.connect(_char_entered)
 
 func _char_entered(body: Node2D):
 	if body is Player:
-		SceneManager.player_spawn = player_spawn_position
+		SceneManager.player_spawn = player_other_scene_spawn
 		print(SceneManager.player_spawn)
 
 		get_tree().change_scene_to_file.call_deferred(new_scene)
